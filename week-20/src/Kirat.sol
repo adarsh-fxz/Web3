b802cc368043345fd4d04b219e21c8d8063b35ca
Adarsh Gupta <gadarsh318@gmail.com>
2025-11-10 19:20:43 +0545

    feat: add Ownable, mint, and burn features to Kirat and WKirat contracts

--- week-20/src/Kirat.sol
+++ week-20/src/Kirat.sol
@@ -2,7 +2,12 @@
 pragma solidity ^0.8.13;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Kirat is ERC20 {
-    constructor() ERC20("Kirat", "KIR") {}
+contract Kirat is ERC20, Ownable {
+    constructor() ERC20("Kirat", "KIR") Ownable(msg.sender) {}
+
+    function mint(address _to, uint256 _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
 }
