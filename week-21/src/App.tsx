9af5ed71da908999ec8d1e67f3569c2cbf5120b0
Adarsh Gupta <gadarsh318@gmail.com>
2025-11-12 04:55:59 +0545

    feat: add TotalSupply component to show contract supply

--- week-21/src/App.tsx
+++ week-21/src/App.tsx
@@ -4,6 +4,7 @@ import {
   useConnect,
   useConnectors,
   useDisconnect,
+  useReadContract,
   WagmiProvider,
 } from "wagmi";
 import { config } from "./config";
@@ -16,9 +17,37 @@ function App() {
     <WagmiProvider config={config}>
       <QueryClientProvider client={client}>
         <ConnectWallet />
+        <TotalSupply />
       </QueryClientProvider>
     </WagmiProvider>
   );
+}
+
+function TotalSupply() {
+  const { data, isLoading, error } = useReadContract({
+    address: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+    abi: [
+      {
+        constant: true,
+        inputs: [],
+        name: "totalSupply",
+        outputs: [
+          {
+            name: "",
+            type: "uint256",
+          },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+    ],
+    functionName: "totalSupply",
+  });
+
+  if (isLoading) return <div>Loading total supply...</div>;
+  if (error) return <div>Error fetching total supply</div>;
+  return <div>Total Supply: {data?.toString()}</div>;
 }
 
 function ConnectWallet() {
