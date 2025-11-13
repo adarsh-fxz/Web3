8c465c5b963b5c244e2643a2b2d92d4822e0888c
Adarsh Gupta <gadarsh318@gmail.com>
2025-11-14 01:38:43 +0545

    feat: add upgradable OrcaCoin contract + tests

--- week-23/src/OrcaCoinContract.sol
+++ week-23/src/OrcaCoinContract.sol
@@ -1,4 +1,27 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract OrcaCoinContract {}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract OrcaCoinContract is ERC20 {
+    address stakingContract;
+    address owner;
+
+    constructor(address _stakingContract) ERC20("OrcaCoin", "ORCA") {
+        stakingContract = _stakingContract;
+        owner = msg.sender;
+    }
+
+    function mint(address to, uint256 amount) public {
+        require(
+            msg.sender == stakingContract,
+            "Only staking contract can mint"
+        );
+        _mint(to, amount);
+    }
+
+    function updateStakingContract(address _stakingContract) public {
+        require(msg.sender == owner);
+        stakingContract = _stakingContract;
+    }
+}
--- week-23/test/OrcaCoinContract.t.sol
+++ week-23/test/OrcaCoinContract.t.sol
@@ -9,6 +9,28 @@ contract TestContract is Test {
     OrcaCoinContract c;
 
     function setUp() public {
-        c = new OrcaCoinContract();
+        c = new OrcaCoinContract(address(this));
+    }
+
+    function testInitialSupply() public view {
+        assert(c.totalSupply() == 0);
+    }
+
+    function test_RevertIf_MintFails() public {
+        vm.expectRevert("Only staking contract can mint");
+        vm.prank(0x90a254BEc9164E08a1990F5B90757DE6b3CF5696);
+        c.mint(0x90a254BEc9164E08a1990F5B90757DE6b3CF5696, 1000);
+    }
+
+    function testMint() public {
+        c.mint(0x90a254BEc9164E08a1990F5B90757DE6b3CF5696, 1000);
+        assert(c.balanceOf(0x90a254BEc9164E08a1990F5B90757DE6b3CF5696) == 1000);
+    }
+
+    function testChangeStakingContract() public {
+        c.updateStakingContract(0x90a254BEc9164E08a1990F5B90757DE6b3CF5696);
+        vm.prank(0x90a254BEc9164E08a1990F5B90757DE6b3CF5696);
+        c.mint(0x90a254BEc9164E08a1990F5B90757DE6b3CF5696, 1000);
+        assert(c.balanceOf(0x90a254BEc9164E08a1990F5B90757DE6b3CF5696) == 1000);
     }
 }
