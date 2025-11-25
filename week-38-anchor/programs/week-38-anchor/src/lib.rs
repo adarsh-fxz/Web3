c0b5648a893fc7dff4f6f8aa695e02165cf2abd3
Adarsh Gupta <gadarsh318@gmail.com>
2025-11-25 16:15:59 +0545

    add calculator contract

--- week-38-anchor/programs/week-38-anchor/src/lib.rs
+++ week-38-anchor/programs/week-38-anchor/src/lib.rs
@@ -6,11 +6,49 @@ declare_id!("HshsVx4KFHEG1C9eAv9WNykn1FZegg2A4FDUs9ndsTxE");
 pub mod week_38_anchor {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn init(ctx: Context<Initialize>, init_value: u32) -> Result<()> {
+        ctx.accounts.account.num = init_value;
+
+        Ok(())
+    }
+
+    pub fn double(ctx: Context<Double>) -> Result<()> {
+        ctx.accounts.account.num *= 2;
+        Ok(())
+    }
+
+    pub fn add(ctx: Context<Add>, num: u32) -> Result<()> {
+        ctx.accounts.account.num += num;
         Ok(())
     }
 }
 
+#[account]
+pub struct DataShape {
+    pub num: u32,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = 8 + 4)]
+    pub account: Account<'info, DataShape>,
+    pub system_program: Program<'info, System>,
+    #[account(mut)]
+    signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Double<'info> {
+    #[account(mut)]
+    pub account: Account<'info, DataShape>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Add<'info> {
+    #[account(mut)]
+    pub account: Account<'info, DataShape>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
--- week-38-anchor/tests/week-38-anchor.ts
+++ week-38-anchor/tests/week-38-anchor.ts
@@ -1,16 +1,28 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Week38Anchor } from "../target/types/week_38_anchor";
+import assert from "assert";
 
 describe("week-38-anchor", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
+  const newAccount = anchor.web3.Keypair.generate();
+
   const program = anchor.workspace.week38Anchor as Program<Week38Anchor>;
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods
+      .init(10)
+      .accounts({
+        signer: anchor.getProvider().wallet.publicKey,
+        account: newAccount.publicKey,
+      })
+      .signers([newAccount])
+      .rpc();
     console.log("Your transaction signature", tx);
+    const account = await program.account.dataShape.fetch(newAccount.publicKey);
+    assert(account.num == 10);
   });
 });
